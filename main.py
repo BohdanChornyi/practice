ed98fb3270a938735e17af19f80affba2b81b814
Bohdan Chornyi <justblackrid97@gmail.com>
2026-08-23 13:16:52 +0300

    Add gitignore and update main

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 # main.py
 
+#  Самая важная схема:
+
+#                 изменил файл
+#                      ↓
+#                   git diff          ← вижу изменения в рабочей папке
+#
+#                    git add
+#                      ↓
+#                   git diff          ← пусто
+#              git diff --staged      ← изменения здесь
+#
+#                   git commit
+#                      ↓
+#          изменения сохранены в истории
+
+
 name = input("Enter your name: ")
 
 print(f"Hello, {name.title()}!")
